9c5520f28c7e280a5c35cb08924b8cf421a60127
Ardit <arditymeri7@gmail.com>
2022-11-18 11:34:54 +0000

    added tiled to script

--- op1_script.sh
+++ op1_script.sh
@@ -46,51 +46,44 @@ else
 fi
 
 
-# EXE2=run2.exe
-# rm -f ${EXE2}
-# icc -qopenmp -O3 op2_main.c tiledop2.c -o run2.exe -std:c99
-# if test -x $EXE2; then
-#       # set number of threads
-#       export OMP_NUM_THREADS=1 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'
-
-#       export OMP_NUM_THREADS=2 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'
-
-#       export OMP_NUM_THREADS=4 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'
-
-#       export OMP_NUM_THREADS=8 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'
-
-#       export OMP_NUM_THREADS=16 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'
-
-#       export OMP_NUM_THREADS=32 # if '-c' not used then default to 1
-#       echo using ${OMP_NUM_THREADS} OpenMP threads
-#       # run multiple times
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'; echo
-#       ./${EXE2};echo '---'
-
-# else
-#      echo $SRC did not built to $EXE2
-# fi
-
-
+EXE2=run2.exe
+rm -f ${EXE2}
+icc -qopenmp -O3 op1_main.c op1_a.c -o run2.exe -std:c99 
+if test -x $EXE; then
+      # set number of threads
+      export OMP_NUM_THREADS=1 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'	
+      export OMP_NUM_THREADS=2 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'
+      export OMP_NUM_THREADS=4 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times	
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'
+      export OMP_NUM_THREADS=8 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'
+      export OMP_NUM_THREADS=16 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'
+      export OMP_NUM_THREADS=32 # if '-c' not used then default to 1
+      echo using ${OMP_NUM_THREADS} OpenMP threads
+      # run multiple times
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'; echo
+      ./${EXE} input_64_512_960.dat kernel_5.dat output_64_512_960.dat;echo '---'
